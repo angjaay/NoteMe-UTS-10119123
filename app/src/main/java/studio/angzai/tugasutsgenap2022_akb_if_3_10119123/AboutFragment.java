7e7b2041537a6241b6789f4bcb694951e889df81
angjaay <angga.angzai@gmail.com>
2022-06-02 12:21:32 +0700

    NoteMe first commit

---
+++ app/src/main/java/studio/angzai/tugasutsgenap2022_akb_if_3_10119123/AboutFragment.java
@@ -0,0 +1,34 @@
+package studio.angzai.tugasutsgenap2022_akb_if_3_10119123;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+//Nama : ANGGA CAHYA ABADI
+//NIM : 10119123
+//Kelas : IF-3
+
+public class AboutFragment extends Fragment {
+
+    public AboutFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+}
---
+++ app/src/main/java/studio/angzai/tugasutsgenap2022_akb_if_3_10119123/MainActivity.java
@@ -0,0 +1,69 @@
+package studio.angzai.tugasutsgenap2022_akb_if_3_10119123;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import nl.joery.animatedbottombar.AnimatedBottomBar;
+
+//Nama : ANGGA CAHYA ABADI
+//NIM : 10119123
+//Kelas : IF-3
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    AnimatedBottomBar animatedBottomBar;
+    FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        animatedBottomBar = findViewById(R.id.animatedBottomBar);
+
+        if (savedInstanceState==null){
+            animatedBottomBar.selectTabById(R.id.home, true);
+            fragmentManager = getSupportFragmentManager();
+            HomeFragment homeFragment = new HomeFragment();
+            fragmentManager.beginTransaction().replace(R.id.fragment_container, homeFragment)
+                    .commit();
+        }
+
+        animatedBottomBar.setOnTabSelectListener(new AnimatedBottomBar.OnTabSelectListener() {
+            @Override
+            public void onTabSelected(int lastIndex, @Nullable AnimatedBottomBar.Tab lastTab, int newIndex, @NonNull AnimatedBottomBar.Tab newTab) {
+                Fragment fragment = null;
+                switch (newTab.getId()){
+                    case R.id.home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.profile:
+                        fragment = new ProfileFragment();
+                        break;
+                    case  R.id.about:
+                        fragment = new AboutFragment();
+                        break;
+                }
+
+                if (fragment != null){
+                    fragmentManager = getSupportFragmentManager();
+                    fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment)
+                            .commit();
+                }else{
+                    Log.e(TAG, "Error in creating fragment");
+                }
+            }
+
+            @Override
+            public void onTabReselected(int lastIndex, @NonNull AnimatedBottomBar.Tab lastTab) {
+
+            }
+        });
+    }
+}
