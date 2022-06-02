6f3bc61fbf8503580d9ef0974734de8d7a4a5da2
angjaay <angga.angzai@gmail.com>
2022-06-02 22:54:08 +0700

    Add adapter for slider

--- app/src/main/java/studio/angzai/tugasutsgenap2022_akb_if_3_10119123/Adapter.java
+++ app/src/main/java/studio/angzai/tugasutsgenap2022_akb_if_3_10119123/Adapter.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.PagerAdapter;
 
 public class Adapter extends PagerAdapter {
